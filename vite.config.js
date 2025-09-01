266a5145d14c55684241b1c310266620a6102fc8
dbogosavljev <dbogosavljev@miteksystems.com>
2025-09-01 15:52:26 +0200

    fix: correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cool-animation-portfolio/'
+  base: '/cool-animation-portfolio/', // 👈 required for GitHub Pages
 })
