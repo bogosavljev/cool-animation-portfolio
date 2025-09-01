568d30ecd91fc5dd17645cb71dbe8db12c44193c
dbogosavljev <dbogosavljev@miteksystems.com>
2025-09-01 16:11:43 +0200

    fix: update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    visualizer({
+      filename: 'stats.html',   // output file
+      template: 'treemap',      // sunburst | treemap | network
+      gzipSize: true,
+      brotliSize: true,
+      open: true                // auto-open in browser after build
+    })
+  ],
   base: '/cool-animation-portfolio/', // 👈 required for GitHub Pages
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          // Split React libraries into their own chunk
-          react: ['react', 'react-dom'],
-
-          // Example: if you use framer-motion, split it too
-          animation: ['framer-motion'],
-
-          // Example: if you use three.js, split it too
-          // three: ['three']
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // get the package name
+            const parts = id.toString().split('node_modules/')[1].split('/')
+            const pkgName = parts[0].startsWith('@') ? parts[0] + '/' + parts[1] : parts[0]
+            return pkgName
+          }
         }
       }
     }
