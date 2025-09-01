0e83849f490f29b46c494458877b3af58a1acc48
dbogosavljev <dbogosavljev@miteksystems.com>
2025-09-01 15:58:18 +0200

    fix: added manualChunks to vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/cool-animation-portfolio/', // 👈 required for GitHub Pages
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split React libraries into their own chunk
+          react: ['react', 'react-dom'],
+
+          // Example: if you use framer-motion, split it too
+          animation: ['framer-motion'],
+
+          // Example: if you use three.js, split it too
+          // three: ['three']
+        }
+      }
+    }
+  }
 })
